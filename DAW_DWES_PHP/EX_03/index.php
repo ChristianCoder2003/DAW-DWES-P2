e9d826640992f7aaf7c3f3b5457561aa075c0e90
ChristianCoder2003 <113515397+ChristianCoder2003@users.noreply.github.com>
2022-09-17 19:24:55 +0200

    Create index.php

---
+++ DAW_DWES_PHP/EX_03/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Ejercicio 3</title>
+    </head>
+    <body>
+        <div style="border: 1px solid">
+            <?php
+            $countries = ["ESP" => ["Name" => "España", "Code" => "710"],
+                "AND" => ["Name" => "Andorra", "Code" => "020"],
+                "ECU" => ["Name" => "Ecuador", "Code" => "218"],
+                "BGR" => ["Name" => "Bulgaria", "Code" => "100"],
+                "BRA" => ["Name" => "Brazil", "Code" => "076"],
+                "GAB" => ["Name" => "Gabon", "Code" => "266"],
+                "CAN" => ["Name" => "Canada", "Code" => "124"],
+                "JAM" => ["Name" => "Jamaica", "Code" => "388"],
+                "COG" => ["Name" => "Andorra", "Code" => "178"],
+                "CPV" => ["Name" => "Cabo Verde", "Code" => "132    "]
+            ];
+            foreach ($countries as $country => ["Name"=> $name, "Code"=> $code]) {
+                echo "El pais " . $name . " utiliza el codigo alfa-3 " . $country . " con codigo numerico: " . $code . "<br>";
+            }
+            ?>
+        </div>
+    </body>
+</html>
