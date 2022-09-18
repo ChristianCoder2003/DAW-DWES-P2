5fc424325823d4ba141a5c162eacd23e15d7399a
ChristianCoder2003 <113515397+ChristianCoder2003@users.noreply.github.com>
2022-09-18 14:43:33 +0200

    Add files via upload

--- DAW_DWES_PHP/EX_11/index.php
+++ DAW_DWES_PHP/EX_11/index.php
@@ -5,6 +5,6 @@ $wordToCompare = "HOLA";
 function IsStringUpperCase(String $word):bool {
     return strtoupper($word) == $word;
 }
-
+//json_encode sirve para mostrar como texto el resultado booleano de IsStringUpperCase()
 echo "Todos los carácteres de la palabra  \"$wordToCompare\" están en mayúsculas?: "
     . json_encode(IsStringUpperCase($wordToCompare));
