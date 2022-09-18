6e09a27649690fef1e6b57e6b69588212bb07b5c
ChristianCoder2003 <113515397+ChristianCoder2003@users.noreply.github.com>
2022-09-18 14:45:24 +0200

    Add files via upload

--- DAW_DWES_PHP/EX_12/index.php
+++ DAW_DWES_PHP/EX_12/index.php
@@ -1,6 +1,8 @@
 <?php
     declare(strict_types=1);
     $number = 7;
+    /*Con el bucle for(), iteramos desde el número 2 hasta la mitad del número indicado (ya que dividiendo por un número
+    mayor daría decimal siempre), y miramos si el residuo es 0; si lo es, no es un número primo, ya que sería un divisor*/
     for ($i = 2; $i <= $number / 2; $i++) {
         if ($number % $i == 0) {
             echo "$number no es un numero primo";
