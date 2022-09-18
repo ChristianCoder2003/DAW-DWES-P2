6f8bfd23e77d7189ae007fea8291d36904ad6a3c
ChristianCoder2003 <113515397+ChristianCoder2003@users.noreply.github.com>
2022-09-18 14:34:34 +0200

    Add files via upload

--- DAW_DWES_PHP/EX_04/index.php
+++ DAW_DWES_PHP/EX_04/index.php
@@ -1,11 +1,15 @@
 <?php
     $words = ["Hola", "Caballero", "Triste", "Aun", "Electricidad"];
+    //Inicializar la palabra con máxima y mínima longitud como la primera palabra seleccionada.
     $minLength = $words[0];
     $maxLength = $words[0];
     echo "<pre>";
     print_r($words);
     echo "</pre>";
     for ($i = 1; $i < count($words); $i++) {
+        /*Mira si la longitud de la palabra de la iteración actual es menor que la longitud de la palabra más corta
+        guardada; si lo es, guarda esa palabra como la nueva mínima. Lo mismo con la palabra más larga, mirando si la
+        palabra actual es más larga que la longitud de la palabra más larga guardada*/
         if (strlen($words[$i]) <= strlen($minLength)) {
             $minLength = $words[$i];
         } elseif (strlen($words[$i]) >= strlen($maxLength)) {
