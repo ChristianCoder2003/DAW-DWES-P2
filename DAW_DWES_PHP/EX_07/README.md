35a5698c19cde2cd775e814929c5524231b35009
ChristianCoder2003 <113515397+ChristianCoder2003@users.noreply.github.com>
2022-09-18 14:39:30 +0200

    Add files via upload

---
+++ DAW_DWES_PHP/EX_07/README.md
@@ -0,0 +1,9 @@
+## ## Ejercicio 7
+#### Todo funciona con éxito
+### Explicación:
+
+Dentro de una tabla de html, se crea un bucle for() que haga 8 iteraciones, para que la tabla contenga
+8 filas (tr). Dentro, se crea otro bucle for(), pero esta vez con 4 iteraciones, ya que por cada
+iteración se dibujan 2 columnas (es decir, 2 casillas, elemento td), una blanca y la otra negra.
+De esta forma se puede controlar que, dependiendo de si el número de la fila ($i) es par o impar, se pinte una casilla
+blanca y otra negra, o a la inversa.
--- DAW_DWES_PHP/EX_07/index.php
+++ DAW_DWES_PHP/EX_07/index.php
@@ -8,6 +8,8 @@
             <?php for ($i = 0; $i < 8; $i++): ?>
                 <tr>
                     <?php for ($j = 0; $j < 4; $j++):
+                        //se realiza un bucle for() anidado, con 8 filas y 4 columnas, ya que por cada columna dibujamos
+                        // 2 casillas (blanca y negra)
                         if ($i % 2 != 0): ?>
                             <td style="background-color: black; padding: 30px; border: 1px solid"></td>
                             <td style="background-color: white; padding: 30px; border: 1px solid"></td>
